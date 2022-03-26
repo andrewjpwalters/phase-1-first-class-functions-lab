c96b864ab868cafa32894db4623b1e43c7015329
Andrew Walters <andrewjpwalters@gmail.com>
2022-03-25 23:28:05 -0500

    commit tree 3cc4932ab4da29c0bcee7e52ac3a0db520c55ab8

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = drivers => drivers.slice(0, 2);
+
+const returnLastTwoDrivers = drivers => drivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiply) {
+    return function (fare) {
+        return multiply * fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, cb) {
+    if (cb === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers)
+    } else {
+        return returnLastTwoDrivers(drivers)
+    }
+}
